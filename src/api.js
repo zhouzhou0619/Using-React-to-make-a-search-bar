2dfca2099eb558ea7bbce2a396373acde53d3957
zhouzhou <zhouzzhou0619@gmail.com>
2023-05-26 00:15:16 +1000

    26/05/2023

--- src/api.js
+++ src/api.js
@@ -1,18 +1,16 @@
 import axios from 'axios';
 
-const searchImages = async () => {
+const searchImages = async term => {
     const response = await axios.get('https://api.unsplash.com/search/photos', {
         headers: {
             Authorization: 'Client-ID 6HkMNQNRXwmXHujT41cL0qIlHdFYFAPoJ0CR6xB1HGs',
         },
         params: {
-            query: 'cars',
+            query: term,
         },
     });
 
-    console.log(response);
-
-    return response;
+    return response.data.results;
 };
 
 export default searchImages;
